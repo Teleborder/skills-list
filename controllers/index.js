47943a4c7bfd3a42dea08ad79cb27e447bfce334
Trey Griffith <trey.griffith@gmail.com>
2015-04-01 23:19:29 -0700

    add countries index route

---
+++ controllers/index.js
@@ -0,0 +1,1 @@
+exports.countries = require('./countries');
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var controllers = require('./controllers');
 
 function routes(app) {
+  app.get('/countries', controllers.index);
 }
 
 module.exports = routes;
